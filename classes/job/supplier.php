e31bb1ab345fa95730f81bf5430c44dc09f7c1f4
Sági-Kazár Márk <webmaster@firstcomputer.hu>
2013-10-18 22:48:30 +0200

    Exception

--- classes/job/supplier.php
+++ classes/job/supplier.php
@@ -24,7 +24,7 @@ class Job_Supplier
 		}
 		else
 		{
-			$log->error('Runtime error during execution of job: ' . $payload['job'] . ' (' .get_class($e) . ': ' . $e->getMessage() .  ')', array('payload' => $payload));
+			$log->error('Runtime error in file ' . $e->getFile() . ' on line ' . $e->getLine() . ' during execution of job: ' . $payload['job'] . ' (' .get_class($e) . ': ' . $e->getMessage() .  ')', array('payload' => $payload));
 		}
 		return true;
 	}
